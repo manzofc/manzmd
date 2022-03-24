3ce41a0598b87c02acb771af54429d10e80b621e
manzofc <manzok602@gmail.com>
2022-03-25 01:03:08 +0700

    manzmd

---
+++ ryudata/sewa.js
@@ -0,0 +1,11 @@
+exports.sewa = () => {
+	return`𝗢 𝗣 𝗘 𝗡  𝗦 𝗘 𝗪 𝗔  𝗕 𝗢 𝗧
+
+• *1 Minggu : 3.000 ( 5K )*
+• *1 Bulan : 5.000 ( 5K )*
+• *Permanen : 7.000 ( 7K )*
+• *Join Murid Bot : 20.000 ( 20K )*
+
+Minat Ketik .owner
+`
+}
---
+++ shop/allpayment.js
@@ -0,0 +1,28 @@
+const allpayment = (prefix) => {
+return `┏━━━━━━━━━━━━━━━━━━━━
+┃           *PAYMENT*
+┣━━━━━━━━━━━━━━━━━━━━
+┣━━⊱❉ *💰 E-WALLET* ❉⊰━━━✿
+┃   
+┣━⊱ *GOPAY*
+┣⊱ 085707278351
+┣━━✿ NAMA GOPAY ${ownername}
+┃
+┣━⊱ *DANA*
+┣⊱ MINTA QRIS ALLPAY DI OWNER
+┣━━✿ NAMA DANA ${ownername}
+┃
+┣━⊱ *PULSA*
+┣⊱ 083830930651
+┣━━✿ NAMA PULSA ${ownername}
+┃
+┗━━━━━━━━━━━━━━━━━━━━
+
+Kirim Foto Bukti Tf Menggunakan !donetf
+
+NOTE! : Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
